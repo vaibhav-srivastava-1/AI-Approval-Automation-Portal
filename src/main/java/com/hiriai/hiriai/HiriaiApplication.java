04d5ce87c73a2ec877f3d865a448915154cf7e3a
Vaibhav <vaibhavsriastava7985@gmail.com>
2026-05-12 14:32:01 +0530

    Initial commit - project files

---
+++ src/main/java/com/hiriai/hiriai/HiriaiApplication.java
@@ -0,0 +1,13 @@
+package com.hiriai.hiriai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiriaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiriaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiriai/hiriai/HiriaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiriai.hiriai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiriaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
